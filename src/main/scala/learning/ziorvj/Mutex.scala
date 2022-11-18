b4635477e6d3ba2db27ace6ad07dfb1662fbdad4
Mih4lych <mih4illl@gmail.com>
2022-11-18 08:17:39 +0300

    Mutex.

--- src/main/scala/learning/ziorvj/Mutex.scala
+++ src/main/scala/learning/ziorvj/Mutex.scala
@@ -1,6 +1,8 @@
 package learning.ziorvj
 
+import sun.misc.Signal
 import zio._
+
 import scala.collection.immutable.Queue
 import utils._
 
@@ -15,9 +17,9 @@ object Mutex {
 
   case class State(locked: Boolean = false, waiting: Queue[Signal] = Queue.empty)
 
-  val innerState = State()
+  val beginState = State()
 
-  def make: UIO[Mutex] = Ref.make(innerState).map { state =>
+  def make: UIO[Mutex] = Ref.make(beginState).map { state =>
     new Mutex {
 
       /*
@@ -25,7 +27,14 @@ object Mutex {
         - if the mutex is unlocked, lock it
         - if the mutex is locked, state becomes (true, queue + new signal) and WAIT on that signal
        */
-      override def acquire: UIO[Unit] = ???
+      override def acquire: UIO[Unit] = {
+        Promise.make[Nothing, Unit].flatMap { signal =>
+          state.modify {
+            case State(false, _) => ZIO.unit -> State(true, Queue())
+            case State(true, waiting) => signal.await -> State(true, waiting.enqueue(signal))
+          }.flatten
+        }
+      }
 
       /*
               Change the state of the Ref
@@ -34,7 +43,18 @@ object Mutex {
                 - if the queue is empty, unlock the mutex
                 - if the queue is non-empty, take a signal out of the queue and complete it
              */
-      override def release: UIO[Unit] = ???
+      override def release: UIO[Unit] = {
+        state.modify {
+          case State(false, _) => ZIO.unit -> beginState
+          case State(true, waiting) =>
+            if (waiting.isEmpty)
+              ZIO.unit -> beginState
+            else {
+              val (prom, restQueue) = waiting.dequeue
+              prom.succeed(()).unit -> State(true, restQueue)
+            }
+        }.flatten
+      }
     }
   }
 }
@@ -54,20 +74,16 @@ object MutexPlayground extends ZIOAppDefault {
     })
 
   def createTask(id: Int, mutex: Mutex): UIO[Int] = {
-    val task = for {
+    for {
       _ <- ZIO.succeed(s"[task $id] waiting for mutex...").debugThread
       _ <- mutex.acquire
       // critical region start
       _ <- ZIO.succeed(s"[task $id] mutex acquired, working...").debugThread
-      result <- workInCriticalRegion().onInterrupt(mutex.release)
+      result <- workInCriticalRegion()
       _ <- ZIO.succeed(s"[task $id] got result: $result, releasing mutex").debugThread
       // critical region end
       _ <- mutex.release
     } yield result
-
-    task
-      .onInterrupt(ZIO.succeed(s"[task $id] was interrupted.").debugThread)
-      .onError(cause => ZIO.succeed(s"[task $id] ended in error: $cause"))
   }
 
   def demoLockingTasks() = for {
@@ -75,14 +91,5 @@ object MutexPlayground extends ZIOAppDefault {
     _ <- ZIO.collectAllParDiscard((1 to 10).toList.map(i => createTask(i, mutex)))
   } yield ()
 
-  def createInterruptingTask(id: Int, mutex: Mutex): UIO[Int] =
-    if (id % 2 == 0)
-      createTask(id, mutex)
-    else for {
-      fib <- createTask(id, mutex).fork
-      _ <- ZIO.sleep(2500.millis) *> ZIO.succeed(s"interrupting task $id").debugThread *> fib.interrupt
-      result <- fib.join
-    } yield result
-
-  def run = ???
+  def run = demoLockingTasks()
 }
