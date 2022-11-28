d5dbfec4f35182512b74acb189698c37fa2abd0e
Mih4lych <mih4illl@gmail.com>
2022-11-28 07:38:18 +0300

    Solutions of the algorithm course: days 10.

--- src/main/scala/leetcode/LeetCode.scala
+++ src/main/scala/leetcode/LeetCode.scala
@@ -1149,4 +1149,17 @@ object LeetCode extends App {
         null
     }
   }
+
+  def mergeTwoLists(list1: ListNode, list2: ListNode): ListNode = {
+    if (list1 == null) list2
+    else if (list2 == null) list1
+    else if (list1.x < list2.x) {
+      list1.next = mergeTwoLists(list1.next, list2)
+      list1
+    }
+    else {
+      list2.next = mergeTwoLists(list1, list2.next)
+      list2
+    }
+  }
 }
