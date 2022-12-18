796129e43586a503079bd4841f799a0a9505cefb
Mih4lych <mih4illl@gmail.com>
2022-12-18 20:25:30 +0300

    Solutions of the second part book tasks chapter 1.

--- src/main/scala/learning/cats/bookexercises/CatsExFromFirstCh.scala
+++ src/main/scala/learning/cats/bookexercises/CatsExFromFirstCh.scala
@@ -1,5 +1,12 @@
 package learning.cats.bookexercises
 
+import cats.{Eq, Show}
+import cats.syntax.show._
+import cats.syntax.eq._
+import cats.instances.int._
+import cats.instances.string._
+import cats.instances.option._
+
 object CatsExFromFirstCh {
   trait Printable[A] {
     def format(value: A): String
@@ -17,6 +24,9 @@ object CatsExFromFirstCh {
     implicit object PrinterForCat extends Printable[Cat] {
       override def format(value: Cat): String = s"${value.name} is a ${value.age} year-old ${value.color} cat."
     }
+
+    implicit val showForCat: Show[Cat] = Show.show[Cat] { cat => s"${cat.name.show} is a ${cat.age.show} year-old ${cat.color.show} cat." }
+    implicit val eqForCat: Eq[Cat] = Eq.instance[Cat] { (cat1, cat2) => cat1.age === cat2.age && cat1.name === cat2.name && cat1.color === cat2.color }
   }
 
   object Printable {
@@ -47,5 +57,15 @@ object CatsExFromFirstCh {
     val cat = Cat(name = "Sam", age = 5, color = "black")
 
     cat.print()
+
+    println(cat.show)
+
+    val cat1 = Cat("Garfield", 38, "orange and black")
+    val cat2 = Cat("Heathcliff", 33, "orange and black")
+    val optionCat1 = Option(cat1)
+    val optionCat2 = Option.empty[Cat]
+
+    println(cat1 === cat2)
+    println(optionCat1 === optionCat2)
   }
 }
