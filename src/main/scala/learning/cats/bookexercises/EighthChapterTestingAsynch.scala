3a39226a98e977bf15c2af9ec5314c1d34a7baac
Mih4lych <mih4illl@gmail.com>
2023-01-22 19:33:02 +0300

    Solutions of the task from chapter 8.

---
+++ src/main/scala/learning/cats/bookexercises/EighthChapterTestingAsynch.scala
@@ -0,0 +1,47 @@
+package learning.cats.bookexercises
+
+import cats.{Applicative, Id}
+import cats.instances.future._
+import cats.instances.list._
+import cats.syntax.traverse._
+import cats.syntax.functor._
+
+import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, Future}
+
+object EighthChapterTestingAsynch extends App {
+  implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
+
+  trait UptimeClient[F[_]] {
+    def getUptime(hostname: String): F[Int]
+  }
+
+  class UptimeService[F[_] : Applicative](client: UptimeClient[F]) {
+    def getTotalUptime(hostnames: List[String]): F[Int] =
+      hostnames.traverse(client.getUptime).map(_.sum)
+  }
+
+  /*trait RealUptimeClient extends UptimeClient[Future] {
+    def getUptime(hostname: String): Future[Int]
+  }
+  trait TestUptimeClient extends UptimeClient[Id] {
+    def getUptime(hostname: String): Id[Int]
+  }*/
+
+  class TestUptimeClient(hosts: Map[String, Int]) extends UptimeClient[Id] {
+    def getUptime(hostname: String): Id[Int] =
+      hosts.getOrElse(hostname, 0)
+  }
+
+  def testTotalUptime(): Unit = {
+    val hosts = Map("host1" -> 10, "host2" -> 6)
+    val client = new TestUptimeClient(hosts)
+    val service = new UptimeService(client)
+    val actual = service.getTotalUptime(hosts.keys.toList)
+    val expected = hosts.values.sum
+    assert(actual == expected)
+  }
+
+  testTotalUptime()
+
+}
