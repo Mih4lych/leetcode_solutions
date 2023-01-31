616d9719494c336cdf5ab63cd70a288d62dcd597
Mih4lych <mih4illl@gmail.com>
2023-01-31 07:18:41 +0300

    Some cats checking.

--- src/main/scala/learning/cats/CatsPlayground.scala
+++ src/main/scala/learning/cats/CatsPlayground.scala
@@ -1,9 +1,11 @@
 package learning.cats
 
-import cats.data.Validated
-import cats.{Eval, Monad, Semigroupal}
+import cats.data.{Const, Validated}
+import cats.{Eval, FunctorFilter, Monad, Order, Semigroupal}
 import cats.instances.list._
 import cats.instances.either._
+import cats.syntax.flatMap._
+import cats.syntax.order._
 import cats.syntax.functor._
 object CatsPlayground extends App {
   val list = List(1, 2, 3, 5)
@@ -37,4 +39,22 @@ object CatsPlayground extends App {
   )
 
   println(testSemigroupal)
+
+  case class Cat(name: String, years: Int)
+  object Cat {
+    implicit val testOrder: Order[Cat] = (x, y) => x.years.compare(y.years)
+  }
+
+  println(Cat("asd", 11) >= Cat("ddd", 21))
+
+  println(List(1, 2, 3).fproduct(_ * 4)) //result List((1,4), (2,8), (3,12))
+
+  println(List(1, 2, 3) >> List(3, 4)) //flatmap(_ => List(3, 4))
+  println(List(1, 2, 3) >>= {x => List(x, x * 10)}) //flatmap(x => List(x, x * 10))
+
+  val testFuncFilter = FunctorFilter[List]
+
+  println(testFuncFilter.mapFilter(List(1, 2, 3)){x => if (x % 2 == 0) Some(x) else None})
+
+  val testConst = Const(1)
 }
