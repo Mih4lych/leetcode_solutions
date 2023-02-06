744c3cfc8331d0cba3aba59539c299fc6f9e1960
Mih4lych <mih4illl@gmail.com>
2023-02-06 17:03:47 +0300

    Some cats' testings.

--- build.sbt
+++ build.sbt
@@ -22,3 +22,5 @@ libraryDependencies ++= Seq(
 scalacOptions ++= Seq(
   "-language:higherKinds"
 )
+
+addCompilerPlugin("org.typelevel" % "kind-projector" % "0.13.2" cross CrossVersion.full)
--- src/main/scala/learning/cats/CatsPlayground.scala
+++ src/main/scala/learning/cats/CatsPlayground.scala
@@ -1,12 +1,16 @@
 package learning.cats
 
-import cats.data.{Const, Validated}
-import cats.{Eval, FunctorFilter, Monad, Order, Semigroupal}
+import cats.data.{Const, NonEmptyList, Validated}
+import cats.{Alternative, Applicative, Bifoldable, Contravariant, ContravariantMonoidal, Eval, Foldable, FunctorFilter, Monad, Monoid, Order, Semigroupal, Traverse}
 import cats.instances.list._
 import cats.instances.either._
+import cats.instances.tuple._
+import cats.syntax.bifunctor._
+import cats.syntax.foldable._
 import cats.syntax.flatMap._
 import cats.syntax.order._
 import cats.syntax.functor._
+import cats.syntax.semigroupal._
 object CatsPlayground extends App {
   val list = List(1, 2, 3, 5)
 
@@ -57,4 +61,110 @@ object CatsPlayground extends App {
   println(testFuncFilter.mapFilter(List(1, 2, 3)){x => if (x % 2 == 0) Some(x) else None})
 
   val testConst = Const(1)
+
+  def reduce[A, B, F[_]](fa: F[A])(f: A => B)
+                        (implicit FF: Traverse[F], BB: Monoid[B]): B = {
+    val g: A => Const[B, Unit] = { (a: A) => Const((f(a))) }
+    val x = FF.traverse[Const[B, *], A, Unit](fa)(g)
+    x.getConst
+  }
+
+  reduce[Char, Int, List](List('a', 'b', 'c')) { c: Char => c.toInt }
+
+  //a -> Const[Int, Unit](50) => Cont[Int, List[Unit]]
+
+  trait Decoder[A] {
+    def decode(in: String): Either[Throwable, A]
+  }
+
+  object Decoder {
+    def from[A](f: String => Either[Throwable, A]): Decoder[A] =
+      new Decoder[A] {
+        def decode(in: String) = f(in)
+      }
+  }
+
+  implicit val decoderAlternative = new Alternative[Decoder] {
+    def pure[A](a: A) = Decoder.from(Function.const(Right(a)))
+
+    def empty[A] = Decoder.from(Function.const(Left(new Error("No dice."))))
+
+    def combineK[A](l: Decoder[A], r: Decoder[A]): Decoder[A] =
+      new Decoder[A] {
+        def decode(in: String) = l.decode(in).orElse(r.decode(in))
+      }
+
+    def ap[A, B](ff: Decoder[A => B])(fa: Decoder[A]): Decoder[B] =
+      new Decoder[B] {
+        def decode(in: String) = Applicative[Either[Throwable, *]].ap(ff.decode(in))(fa.decode(in))
+      }
+  }
+
+  val records: List[(Int, Int)] = List((450000, 3), (770000, 4), (990000, 2), (2100, 4), (43300, 3))
+  // records: List[(Int, Int)] = List(
+  //   (450000, 3),
+  //   (770000, 4),
+  //   (990000, 2),
+  //   (2100, 4),
+  //   (43300, 3)
+  // )
+
+  def calculateContributionPerMonth(balance: Int, lifetime: Int) = balance / lifetime
+
+  val result: List[Int] =
+    records.map(
+      record => record.bimap(
+        cents => cents / 100,
+        years => 12 * years
+      )
+    ).map((calculateContributionPerMonth _).tupled)
+  // result: List[Int] = List(125, 160, 412, 0, 12)
+
+  println(Bifoldable[(String, *, *)].bifold(("ad", 1, 2.0)))
+
+  println(NonEmptyList.of(1, 2, 3, 4, 5).coflatMap(_.foldLeft(0)(_ + _)))
+
+  /*object ContravariantMonoidalEx {
+    case class Predicate[A](run: A => Boolean)
+
+    implicit val contravariantMonoidalPredicate: ContravariantMonoidal[Predicate] =
+      new ContravariantMonoidal[Predicate] {
+        def unit: Predicate[Unit] = Predicate[Unit](Function.const(true))
+
+        def product[A, B](fa: Predicate[A], fb: Predicate[B]): Predicate[(A, B)] =
+          Predicate(x => fa.run(x._1) && fb.run(x._2))
+
+        def contramap[A, B](fa: Predicate[A])(f: B => A): Predicate[B] =
+          Predicate(x => fa.run(f(x)))
+      }
+
+    case class Money(value: Long)
+
+    def isEven: Predicate[Long] = Predicate(_ % 2 == 0)
+
+    def isEvenMoney: Predicate[Money] = isEven.contramap(_.value)
+
+    def times2Predicate: Predicate[Long] => Predicate[Long] =
+      ContravariantMonoidal[Predicate].liftContravariant((x: Long) => 2 * x)
+
+    def liftMoney: Predicate[Long] => Predicate[Money] =
+      ContravariantMonoidal[Predicate].liftContravariant(_.value)
+
+    case class Transaction(value: Money, payee: String)
+
+    def isEvan: Predicate[String] = Predicate(_ == "Evan")
+
+    def isGreaterThan50Dollars: Predicate[Money] = liftMoney(Predicate(_ > 50))
+
+    def isEvenPaymentToEvanOfMoreThan50 =
+      (isEvenMoney, isGreaterThan50Dollars, isEvan).contramapN(
+        (trans: Transaction) => (trans.value, trans.value, trans.payee))
+
+    isEvenPaymentToEvanOfMoreThan50.run(Transaction(Money(56), "Evan"))
+  }*/
+
+  val prints: Eval[Unit] = List(Eval.always(println(1)), Eval.always(println(2))).sequence_
+
+  println(prints.value)
+
 }
