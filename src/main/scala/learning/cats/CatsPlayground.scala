930f1ba54458501d261743cb4103d571378c4187
Mih4lych <mih4illl@gmail.com>
2023-02-19 23:43:45 +0300

    Some cats' testings.

--- src/main/scala/learning/cats/CatsPlayground.scala
+++ src/main/scala/learning/cats/CatsPlayground.scala
@@ -1,16 +1,21 @@
 package learning.cats
 
-import cats.data.{Const, NonEmptyList, Validated}
-import cats.{Alternative, Applicative, Bifoldable, Contravariant, ContravariantMonoidal, Eval, Foldable, FunctorFilter, Monad, Monoid, Order, Semigroupal, Traverse}
+import cats.data._
+import cats.implicits.catsSyntaxTuple2Semigroupal
+import cats.{Alternative, Applicative, ApplicativeError, Bifoldable, Contravariant, ContravariantMonoidal, Eval, Foldable, FunctorFilter, Monad, MonadError, Monoid, Order, PartialOrder, Semigroupal, Traverse}
 import cats.instances.list._
 import cats.instances.either._
 import cats.instances.tuple._
 import cats.syntax.bifunctor._
+import cats.syntax.either._
 import cats.syntax.foldable._
 import cats.syntax.flatMap._
 import cats.syntax.order._
+import cats.syntax.parallel._
 import cats.syntax.functor._
 import cats.syntax.semigroupal._
+
+import scala.util.Try
 object CatsPlayground extends App {
   val list = List(1, 2, 3, 5)
 
@@ -38,8 +43,8 @@ object CatsPlayground extends App {
   //println(testEval.value)
   type ErrorOr[A] = Validated[Int, A]
   val testSemigroupal = Semigroupal[ErrorOr].product(
-    Validated.invalid(10),
-    Validated.invalid(20)
+    Validated.valid(10),
+    Validated.valid(20)
   )
 
   println(testSemigroupal)
@@ -166,5 +171,28 @@ object CatsPlayground extends App {
   val prints: Eval[Unit] = List(Eval.always(println(1)), Eval.always(println(2))).sequence_
 
   println(prints.value)
+
+  val testOpAp = MonadError[Try, Throwable]
+
+  println(testOpAp.raiseError(new NoSuchElementException("asd")))
+
+  val testState = for {
+    a <- State.get[Int]
+    _ <- State.set[Int](a + 10)
+    b <- State.get[Int]
+    _ <- State.modify[Int](_ + 43)
+    c <- State.inspect[Int, Int](_ * 2)
+  } yield (a, b, c)
+
+  println(testState.runS(10).value)
+  println(State.pure[Int, Int](10).run(10).value)
+
+  println((List(1, 2), List(2, 3)).tupled)
+  println((List(1, 2), List(2, 3)).parTupled)
+
+  println(List(Either.right(42), Either.left(NonEmptyList.one("Error 1")), Either.left(NonEmptyList.one("Error 2"))).parSequence)
+  println(List(Either.right(42), Either.left(NonEmptyList.one("Error 1")), Either.left(NonEmptyList.one("Error 2"))).sequence_)
+
+  println(List(1, 2, 3).combineAll)
 
 }
