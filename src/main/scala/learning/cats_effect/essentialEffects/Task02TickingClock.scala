dbaeeb802e208a5b103678b38a50d7031cbe6839
Mih4lych <mih4illl@gmail.com>
2023-02-28 18:49:49 +0300

    Another solution.

--- src/main/scala/learning/cats_effect/essentialEffects/Task02TickingClock.scala
+++ src/main/scala/learning/cats_effect/essentialEffects/Task02TickingClock.scala
@@ -6,9 +6,13 @@ import scala.concurrent.duration.DurationInt
 
 object Task02TickingClock extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
-    tickingClock.as(ExitCode.Success)
+    tickingClock2.as(ExitCode.Success)
 
   val tickingClock: IO[Unit] =
     IO.sleep(1.second) >> IO.println(System.currentTimeMillis()) >> tickingClock
+
+  val tickingClock2: IO[Unit] = {
+    (IO.sleep(1.second) >> IO.println(System.currentTimeMillis())).foreverM
+  }
 
 }
