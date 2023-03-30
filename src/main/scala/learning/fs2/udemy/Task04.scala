fdd845be485af59d9e1fb0211da43ac8dc433a7f
Mih4lych <mih4illl@gmail.com>
2023-03-30 18:50:50 +0300

    fs2 solutions.

---
+++ src/main/scala/learning/fs2/udemy/Task04.scala
@@ -0,0 +1,32 @@
+package learning.fs2.udemy
+
+import cats.effect.IO
+import fs2._
+
+import scala.concurrent.duration.FiniteDuration
+
+class Task04 {
+  val data = List.range(1, 100)
+  val pageSize = 20
+
+  def fetchPage(pageNumber: Int): IO[List[Int]] = {
+    val start = pageNumber * pageSize
+    val end = start + pageSize
+
+    IO.println(s"Fetch the page: $pageNumber").as(data.slice(start, end))
+  }
+
+  def fetchAll(): Stream[IO, Int] = {
+    Stream.unfoldEval(0) { pageNumber =>
+      fetchPage(pageNumber).map { data =>
+        if (data.isEmpty) None
+        else Some(Stream.emits(data), pageNumber + 1)
+      }
+    }.flatten
+  }
+
+  def evalEvery[A](t: FiniteDuration)(ef: IO[A]): Stream[IO, A] = {
+    Stream.sleep_[IO](t) ++ Stream.eval(ef) ++ evalEvery(t)(ef)
+
+  }
+}
