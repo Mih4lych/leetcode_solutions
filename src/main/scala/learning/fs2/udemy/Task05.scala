ffef6aabf9e89f329eeff833101cfa6f4d3243ac
Mih4lych <mih4illl@gmail.com>
2023-04-11 16:55:18 +0300

    fs2 solutions.

--- src/main/scala/learning/fs2/udemy/Task05.scala
+++ src/main/scala/learning/fs2/udemy/Task05.scala
@@ -1,7 +1,7 @@
 package learning.fs2.udemy
 
 import cats.effect.{IO, IOApp}
-import fs2.Chunk
+import fs2._
 
 import scala.reflect.ClassTag
 
@@ -13,5 +13,49 @@ object Task05 extends IOApp.Simple {
     Chunk.array(array)
   }
 
-  override def run: IO[Unit] = IO.println(compact(Chunk.singleton(10)))
+  def skipLimit[A](skip: Int, limit: Int)(stream: Stream[IO, A]): Stream[IO, A] = {
+    val p =
+      for {
+        sOpt <- stream.pull.drop(skip)
+        _ <- sOpt match {
+          case Some(s) => s.pull.take(limit)
+          case None => Pull.done
+        }
+      } yield ()
+    p.stream
+  }
+
+  def filter[A](p: A => Boolean): Pipe[Pure, A, A] = s => {
+    def go(s: Stream[Pure, A]): Pull[Pure, A, Unit] = {
+      s.pull.uncons1.flatMap {
+        case Some((value, remStream)) =>
+          if (p(value)) Pull.output1(value) >> go(remStream)
+          else go(remStream)
+        case None => Pull.done
+      }
+    }
+
+    go(s).stream
+  }
+
+  def runnigMax: Pipe[Pure, Int, Int] = s => {
+    s.scanChunksOpt(Int.MinValue) { curMax =>
+      Some { chunk =>
+        val newMax = curMax.max(chunk.foldLeft(Int.MinValue)(_.max(_)))
+
+        (newMax, Chunk.singleton(newMax))
+      }
+    }
+  }
+
+  override def run: IO[Unit] = {
+    IO.println(compact(Chunk.singleton(10)))
+
+    (Stream(1, 2) ++ Stream(3) ++ Stream(4, 5))
+      .through(filter(_ % 2 == 0))
+      .through(runnigMax)
+      .evalMap(IO.println)
+      .compile
+      .drain
+  }
 }
