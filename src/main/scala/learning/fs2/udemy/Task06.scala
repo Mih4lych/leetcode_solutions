ca73a4650a2992fe35d4f8ccebc1a653301391d4
Mih4lych <mih4illl@gmail.com>
2023-04-15 13:15:56 +0300

    Solutions of fs2 tasks.

--- src/main/scala/learning/fs2/udemy/Task06.scala
+++ src/main/scala/learning/fs2/udemy/Task06.scala
@@ -1,7 +1,9 @@
 package learning.fs2.udemy
 
+import cats.Order
 import cats.effect.{IO, IOApp}
-import fs2.concurrent.SignallingRef
+import cats.implicits._
+import fs2.concurrent.{Channel, SignallingRef}
 import fs2.Stream
 
 import scala.concurrent.duration.DurationInt
@@ -33,5 +35,50 @@ object Task06 extends IOApp.Simple {
       createTemperatureSensor(signal, threshold).concurrently(createCooler(signal))
     }.interruptAfter(3.seconds).compile.drain
 
-  override def run: IO[Unit] = program
+  //ex2
+  object Ex2 {
+    sealed trait Measurement
+    case class Temperature(value: Double) extends Measurement
+    case class Humidity(value: Double) extends Measurement
+
+    implicit val ordHum: Order[Humidity] = Order.by(_.value)
+    implicit val ordTem: Order[Temperature] = Order.by(_.value)
+
+    def createTemperatureSensor(alarm: Channel[IO, Measurement], threshold: Temperature): Stream[IO, Nothing] = {
+      Stream
+        .repeatEval(IO(Temperature(Random.between(-40.0, 40.0))))
+        .evalTap(t => IO.println(f"${t.value}%.1f temperature"))
+        .evalMap(t => if (t > threshold) alarm.send(t) else IO.unit)
+        .metered(300.millis)
+        .drain
+    }
+
+    def createHumiditySensor(alarm: Channel[IO, Measurement], threshold: Humidity): Stream[IO, Nothing] = {
+      Stream
+        .repeatEval(IO(Humidity(Random.between(0.0, 100.0))))
+        .evalTap(t => IO.println(f"${t.value}%.1f humidity"))
+        .evalMap(t => if (t > threshold) alarm.send(t) else IO.unit)
+        .metered(100.millis)
+        .drain
+    }
+
+    def createCooler(alarm: Channel[IO, Measurement]): Stream[IO, Nothing] = {
+      alarm
+        .stream
+        .evalMap {
+          case Temperature(value) => IO.println(s"temperature too high $value")
+          case Humidity(value) => IO.println(s"Humidity too high $value")
+        }
+        .drain
+    }
+
+    val program =
+      Stream.eval(Channel.unbounded[IO, Measurement]).flatMap { channel =>
+        Stream(createTemperatureSensor(channel, Temperature(20.0)), createHumiditySensor(channel, Humidity(50.0)))
+          .parJoinUnbounded
+          .concurrently(createCooler(channel))
+      }.interruptAfter(5.seconds).compile.drain
+  }
+
+  override def run: IO[Unit] = Ex2.program
 }
