fe915765e66b9ae3221ce27848c524cdff004354
Mih4lych <mih4illl@gmail.com>
2023-04-15 15:01:00 +0300

    Solution of the fs2 task.

--- src/main/scala/learning/fs2/udemy/Task06.scala
+++ src/main/scala/learning/fs2/udemy/Task06.scala
@@ -1,11 +1,13 @@
 package learning.fs2.udemy
 
 import cats.Order
+import cats.effect.std.Queue
 import cats.effect.{IO, IOApp}
 import cats.implicits._
-import fs2.concurrent.{Channel, SignallingRef}
+import fs2.concurrent.{Channel, SignallingRef, Topic}
 import fs2.Stream
 
+import java.time.LocalDateTime
 import scala.concurrent.duration.DurationInt
 import scala.util.Random
 
@@ -80,5 +82,82 @@ object Task06 extends IOApp.Simple {
       }.interruptAfter(5.seconds).compile.drain
   }
 
-  override def run: IO[Unit] = Ex2.program
+  object Ex3 {
+    case class CarPosition(carId: Long, lat: Double, lng: Double)
+
+    def createCar(carId: Long, topic: Topic[IO, CarPosition]): Stream[IO, Nothing] = {
+      Stream
+        .repeatEval(IO(CarPosition(carId = carId, lat = Random.between(-90.0, 90.0), lng = Random.between(-100.0, 100.0))))
+        .metered(1.second)
+        .through(topic.publish)
+        .drain
+    }
+
+    def createGoogleMapUpdate(topic: Topic[IO, CarPosition]): Stream[IO, Nothing] = {
+      topic
+        .subscribe(10)
+        .evalMap(pos => IO.println(f"Car ${pos.carId} pos ${pos.lat}%.2f ${pos.lng}%.2f"))
+        .drain
+    }
+
+    def createDriverNotifier(topic: Topic[IO, CarPosition], shouldNotify: CarPosition => Boolean, notify: CarPosition => IO[Unit]): Stream[IO, Nothing] = {
+      topic
+        .subscribe(10)
+        .evalMap(pos => if (shouldNotify(pos)) notify(pos) else IO.unit)
+        .drain
+    }
+
+    val program =
+      Stream.eval(Topic[IO, CarPosition]).flatMap { topic =>
+        (Stream.range(1, 10).map(createCar(_, topic)) ++
+          Stream(createGoogleMapUpdate(topic), createDriverNotifier(topic, c => c.carId % 2 == 0, c => IO.println(s"Car ${c.carId} has an even number"))))
+          .parJoinUnbounded
+      }.interruptAfter(3.seconds).compile.drain
+  }
+
+  object Ex4 {
+    trait Controller {
+      def postAccount(customerId: Long, accountType: String, creationDate: LocalDateTime): IO[Unit]
+    }
+
+    class Server(controller: Controller) {
+      def start(): IO[Nothing] = {
+        val program =
+          for {
+            randomWait <- IO(math.abs(Random.nextInt()) % 500)
+            _          <- IO.sleep(randomWait.millis)
+            _          <- controller.postAccount(Random.between(1L, 1000L), (if (Random.nextBoolean()) "ira" else "brokerage"), LocalDateTime.now())
+          } yield ()
+
+        program.foreverM
+      }
+    }
+
+    object PrintController extends Controller {
+      override def postAccount(customerId: Long, accountType: String, creationDate: LocalDateTime): IO[Unit] = {
+        IO.println(s"customer $customerId type $accountType date $creationDate")
+      }
+    }
+
+    case class CreateAccountData(customerId: Long, accountType: String, creationDate: LocalDateTime)
+    class QueueController(queue: Queue[IO, CreateAccountData]) extends Controller {
+      override def postAccount(customerId: Long, accountType: String, creationDate: LocalDateTime): IO[Unit] =
+        queue.offer(CreateAccountData(customerId, accountType, creationDate))
+    }
+
+    val program = new Server(PrintController).start()
+
+    val program2 = Stream.eval(Queue.unbounded[IO, CreateAccountData]).flatMap { queue =>
+      val consumer =
+        Stream
+          .fromQueueUnterminated(queue)
+          .evalMap(IO.println)
+          .drain
+
+      val server = Stream.eval(new Server(new QueueController(queue)).start())
+
+      consumer.merge(server)
+    }.interruptAfter(3.seconds).compile.drain
+  }
+  override def run: IO[Unit] = Ex4.program2
 }
